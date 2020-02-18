337b5c3ae4403620dfdace371abb0d6b5a905044
Matt Luczkowiak <mluczkowiak@us.ibm.com>
2020-02-18 13:05:14 -0600

    end of section 18

---
+++ JQueryExercise/jqueryex.js
@@ -0,0 +1,20 @@
+//set all backgrounds to purple
+function taskOne(){
+	$("div").css("background", "purple")	
+}
+
+//highlight width to 200px
+function taskTwo(){
+	$(".highlight").css("width", "200px")
+}
+
+//set third class to orange border
+function taskThree(){
+	$("#third").css("border", "solid 3px orange")	
+}
+
+//set first div text to pink
+function taskFour(){
+	$("div:first").css("color", "pink")	
+}
+
